fe744db19653fa7894f4f456a77323e501ccbc26
AnhNangCuaEm <102863074+AnhNangCuaEm@users.noreply.github.com>
2025-02-28 11:02:43 +0900

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const songs = [
+    {
+        name: 'Despacito ft. Justin Bieber',
+        artist: 'Luis Fonsi, Daddy Yankee',
+        path: '../assets/music/1.mp3',
+        image: ''
+    },
+    
+]
